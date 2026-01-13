3de41e4e4d96a73b91461f03402812dac4058f76
Jay-2810 <202201187@daiict.ac.in>
2026-01-13 14:31:03 +0530

    calculator with dashboard

--- calculator/src/app/app.routes.ts
+++ calculator/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Calc } from './calc/calc';
+import { Pagenotfound } from './pagenotfound/pagenotfound';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:Home},
+    {path:"dashboard",component:Dashboard},
+    {path:"calc",component:Calc},
+    {path:"**",component:Pagenotfound}
+];
