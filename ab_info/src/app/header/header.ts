5bc32fe568d394f6031412d6b33dc45bd093dab7
Jay-2810 <202201187@daiict.ac.in>
2026-01-22 12:43:46 +0530

    About us page start

--- ab_info/src/app/header/header.ts
+++ ab_info/src/app/header/header.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router,RouterLink, RouterLinkActive, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLinkActive,RouterLink],
+  imports: [RouterLinkActive, RouterLink, NgClass],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
-
+export class Header implements OnInit{
+  constructor(private router:Router){}
+  isHomePage:boolean=false;
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event=>event instanceof NavigationEnd)).subscribe((event:any)=>{
+        this.isHomePage=event.url==='/';
+      });
+  }
 }
