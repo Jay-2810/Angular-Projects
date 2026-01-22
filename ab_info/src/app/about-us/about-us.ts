e7098f664111f284f929bca1b009a4a39eefa4be
Jay-2810 <202201187@daiict.ac.in>
2026-01-22 16:54:57 +0530

    about us completed

--- ab_info/src/app/about-us/about-us.ts
+++ ab_info/src/app/about-us/about-us.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { NgIf } from "@angular/common";
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css',
 })
 export class AboutUs {
+  activeTab: string = 'mission';
 
+  setActiveTab(tabName: string) {
+    this.activeTab = tabName;
+  }
 }
